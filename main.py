b9122ea5038e428f8b148b1df1e095217aa4defc
Sebas Granizo <sebasgranizo@MacBook-Pro-de-Sebas.local>
2021-03-23 15:22:17 -0500

    Refactor

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # Prime Number Calculation
 n = int(input("Input a number: "))
 def is_prime(number = n):
-    factors = 0
-    for i in range(1, number+1):
-        if (number % i) == 0:
-            print(i)
-            print(number % i)
-            factors += 1
-        else:
-            factors += 0
-    if factors > 2:
-        print(f"{number} its not a prime number")
-    else:
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+
+    if is_prime:
         print(f"{number} its a prime number")
+    else:
+        print(f"{number} its not a prime number")
 
 is_prime()
